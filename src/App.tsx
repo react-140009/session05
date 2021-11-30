6b08edc3c17e89174a8d6ebef2e57140e128adae
pasha <pashaie@gmail.com>
2021-11-30 19:15:44 +0330

    App:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { TodoList } from "./components/TodoList";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   return (
     <div className="container">
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && <h1>Test</h1>}
       <TodoList></TodoList>
     </div>
   );
