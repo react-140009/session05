80cafcc0aad15694e910b0d68bd5403c39b9ece9
pasha <pashaie@gmail.com>
2021-12-07 20:50:49 +0330

    reactroe

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useState, createContext } from "react";
 import "./App.css";
 import { ColorSelector } from "./components/ColorSelector";
+import PhotoList from "./components/PhotoList";
 import PostList from "./components/PostList";
 import { TodoList } from "./components/TodoList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export const ColorContext = createContext([]);
 
@@ -12,11 +14,75 @@ function App() {
   return (
     <div className="container">
       <ColorContext.Provider value={[color, setColor] as any}>
-        <ColorSelector></ColorSelector>
-        <button onClick={() => setToggle(!toggle)}>Toggle</button>
-        {toggle && <TodoList></TodoList>}
+        <Router>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <Link className="navbar-brand" to="/">
+                Navbar
+              </Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="/"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/color">
+                      Color
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/todo">
+                      Todo
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/posts">
+                      Posts
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/photos">
+                      Photos
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
 
-        <PostList></PostList>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <button onClick={() => setToggle(!toggle)}>Toggle</button>
+                  {toggle && <h2>Toggle header</h2>}
+                </>
+              }
+            ></Route>
+            <Route path="/color" element={<ColorSelector />} />
+            <Route path="/todo" element={<TodoList />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/photos" element={<PhotoList />} />
+          </Routes>
+        </Router>
       </ColorContext.Provider>
     </div>
   );
---
+++ src/components/PhotoList.tsx
@@ -0,0 +1,84 @@
+import { useFetchData } from "../hooks/useFetchData";
+
+export interface PhotoModel {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+export default function PostList() {
+  const { page, setPage, data, total, loading } =
+    useFetchData<PhotoModel>("photos");
+
+  const prev = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const next = () => {
+    if (page < total / 10) {
+      setPage(page + 1);
+    }
+  };
+
+  return (
+    <>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <button className="page-link" onClick={prev}>
+              Previous
+            </button>
+          </li>
+
+          {new Array(total / 10).fill(0).map((_: any, idx: number) => (
+            <li key={idx} className="page-item">
+              <button className="page-link" onClick={() => setPage(idx + 1)}>
+                {idx + 1}
+              </button>
+            </li>
+          ))}
+
+          <li className="page-item">
+            <button className="page-link" onClick={next}>
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>UserId</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading && (
+            <tr>
+              <td colSpan={100}>
+                <h2>Loading...</h2>
+              </td>
+            </tr>
+          )}
+          {data &&
+            data.map((post) => (
+              <tr key={"post-" + post.id}>
+                <td>{post.id}</td>
+                <td>{post.albumId}</td>
+                <td>{post.title}</td>
+                <td>
+                  <img src={post.thumbnailUrl} />
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
