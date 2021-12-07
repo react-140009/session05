19dd75ed1d1fb8e9d30b39d71f7fe8c54e854477
pasha <pashaie@gmail.com>
2021-12-07 19:27:19 +0330

    components/ColorSelector:

--- src/components/ColorSelector.tsx
+++ src/components/ColorSelector.tsx
@@ -2,7 +2,8 @@ import { useRef, useContext } from "react";
 import { ColorContext } from "../App";
 
 export const ColorSelector = () => {
-  const [color, setColor] = useContext(ColorContext);
+  // const [color, setColor] = useContext(ColorContext);
+  const setColor = useContext(ColorContext)[1];
   const selectRef = useRef<any>();
   const onChnage = (val: string) => {
     (setColor as any)(val);
