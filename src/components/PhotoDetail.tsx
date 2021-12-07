ba81b0f8036fd4e002a2b7388da7d4338e7fd6be
pasha <pashaie@gmail.com>
2021-12-07 21:32:17 +0330

    components/PhotoDetail:

--- src/components/PhotoDetail.tsx
+++ src/components/PhotoDetail.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 export const PhotoDetail = () => {
+  const [data, setData] = useState<any>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos/" + id)
+      .then((resp) => setData(resp.data));
+  });
+
   return (
     <div>
+      {data && (
+        <div className="card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={data.url} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{data.title}</h5>
+            <p className="card-text">{data.title}</p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      )}
       <button>Go Back</button>
     </div>
   );
