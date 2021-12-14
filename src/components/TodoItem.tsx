049b8b1049b91084565455eb34bf7c5379505892
pasha <pashaie@gmail.com>
2021-12-14 20:10:04 +0330

    .

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,16 @@
 import React, { useEffect, memo, useContext } from "react";
 import { ColorContext } from "../App";
 import styles from "./TodoItem.module.css";
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// const template = (a, b) => {}
+const str = `aaa ${1 + 2}`; //(~ tilda, ` backtick);
 
 interface TodoModel {
   id: number;
@@ -23,7 +33,10 @@ export const TodoItem = memo(({ todo, onRemoveTodo, onToggleTodo }: Props) => {
       <td>
         <div className={styles.title}>{todo.id}</div>
       </td>
-      <td style={{ color }}>{todo.title}</td>
+      {/* <td style={{ color }}>{todo.title}</td> */}
+      <td>
+        <Title>{todo.title}</Title>
+      </td>
       <td>
         <button onClick={() => onRemoveTodo(todo.id)}>❌</button>
         <button onClick={() => onToggleTodo(todo.id)}>Toggle</button>
